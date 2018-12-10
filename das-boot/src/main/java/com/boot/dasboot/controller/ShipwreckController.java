38e2c73e2feca4d831653c1e345d7aeaf986c596
michaeldeongreen <michaeldeongreen@gmail.com>
2018-12-10 08:43:18 -0600

    *Check-in from VM

--- das-boot/src/main/java/com/boot/dasboot/controller/ShipwreckController.java
+++ das-boot/src/main/java/com/boot/dasboot/controller/ShipwreckController.java
@@ -1,12 +1,14 @@
 package com.boot.dasboot.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+import com.boot.dasboot.model.Shipwreck;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.boot.dasboot.model.Shipwreck;;
 
 @RestController
 @RequestMapping("api/v1/")
@@ -16,5 +18,26 @@ public class ShipwreckController {
     public List<Shipwreck> list() {
         return ShipwreckStub.list();
     }
+
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.POST)
+    public Shipwreck create(@RequestBody Shipwreck shipwreck) {
+        return ShipwreckStub.create(shipwreck);
+    }
+    
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.GET)
+    public Shipwreck get(@PathVariable Long id) {
+        return ShipwreckStub.get(id);
+    }
+
+    @RequestMapping(value="shipwrecks/{id}", method=RequestMethod.PUT)
+    public Shipwreck requestMethodName(@PathVariable Long id, @RequestBody Shipwreck shipwreck) {
+        return ShipwreckStub.update(id, shipwreck);
+    }
+    
+    @RequestMapping(value="shipwrecks/{id}", method=RequestMethod.DELETE)
+    public Shipwreck requestMethodName(@PathVariable Long id) {
+        return ShipwreckStub.delete(id);
+    }
+    
 
 }
